c309f084a9d72d9baa4c5f1a5d558dc903e0a4e8
TreyLathe <147196984+TreyLathe@users.noreply.github.com>
2024-03-12 09:19:30 -1000

    added new project, small changes to layout

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,10 +4,19 @@ import WeatherDashboard from "../assets/images/WeatherDashboard.jpg";
 import JSQuiz from "../assets/images/JSQuiz.jpg";
 import LogoMaker from "../assets/images/LogoMaker.jpg";
 import EmployeeTracker from "../assets/images/EmployeeTracker.jpg";
+import exambuilder from "../assets/images/exambuilder.jpg";
 
 export default function Portfolio() {
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around', margin: '2rem', }}>
+      <div className="card" style={{ width: "18rem", color: "#FCDEBE",marginBottom: "2rem", backgroundColor: "#5E5768" }}>
+        <img className="card-img-top" src= {exambuilder} alt="Card image cap" style={{ height: "160px" }}></img>
+        <h5 className="card-title">Exam Builder</h5>
+        <p className="card-text">Instructors are able to build, customize, reuse and assign exams, students are able to take exams, review progress and grades.</p>
+        <a href="https://exam-builder.onrender.com" className="btn btn-primary" style={{ backgroundColor: "#928779"}}>Deployed</a>
+        <a href="https://github.com/JessicaDubina/exam-builder" className="btn btn-primary" style={{ backgroundColor: "#928779"}}>GitHub Repository</a>
+      </div>
+      
       <div className="card" style={{ width: "18rem",color: "#FCDEBE", marginBottom: "2rem", backgroundColor: "#5E5768"}}>
         <img className="card-img-top" src= {CourseLogo} alt="course logo" style={{ height: "160px" }}></img>
         <h5 className="card-title">Course Management</h5>
@@ -35,7 +44,7 @@ export default function Portfolio() {
       <div className="card" style={{ width: "18rem",color: "#FCDEBE", marginBottom: "2rem", backgroundColor: "#5E5768" }}>
         <img className="card-img-top" src= {EmployeeTracker} alt="Card image cap" style={{ height: "160px" }}></img>
         <h5 className="card-title">Employee Tracker</h5>
-        <p className="card-text">A command line interface that allows a user to keep track of employees, managers, roles and salaries</p>
+        <p className="card-text">A command line interface that allows a user to keep track of, update and remove employees, managers, roles and salaries</p>
         <a href="#" className="btn btn-primary" style={{ backgroundColor: "#928779"}}>Not Deployed</a>
         <a href="https://github.com/TreyLathe/employee-tracker-chal12" className="btn btn-primary" style={{ backgroundColor: "#928779"}}>GitHub Repository</a>
       </div>
@@ -43,18 +52,12 @@ export default function Portfolio() {
       <div className="card" style={{ width: "18rem", color: "#FCDEBE",marginBottom: "2rem", backgroundColor: "#5E5768" }}>
         <img className="card-img-top" src= {LogoMaker} alt="Card image cap" style={{ height: "160px" }}></img>
         <h5 className="card-title">Logo Maker</h5>
-        <p className="card-text">A command line interfact to create a name initial logo of a chosen shape and color, creating an svg file.</p>
+        <p className="card-text">A command line interfact to create a name initial logo of with choices of text, shape and color, creating an svg file with a unique name for later use.</p>
         <a href="#" className="btn btn-primary" style={{ backgroundColor: "#928779"}}>Not Deployed</a>
         <a href="https://github.com/TreyLathe/logo-creator" className="btn btn-primary" style={{ backgroundColor: "#928779"}}>Github Repository</a>
       </div>
 
-      <div className="card" style={{ width: "18rem", color: "#FCDEBE",marginBottom: "2rem", backgroundColor: "#5E5768" }}>
-        <img className="card-img-top" src= {JSQuiz} alt="Card image cap" style={{ height: "160px" }}></img>
-        <h5 className="card-title">Simple JavaScript Quiz</h5>
-        <p className="card-text">A simple, timed Javascript Quiz to test a beginning users knowledge of Javascript with a high score board.</p>
-        <a href="https://treylathe.github.io/mod-4-challenge/" className="btn btn-primary" style={{ backgroundColor: "#928779"}}>Deployed</a>
-        <a href="https://github.com/TreyLathe/mod-4-challenge" className="btn btn-primary" style={{ backgroundColor: "#928779"}}>GitHub Repository</a>
-      </div>
+      
     </div>
   );
 }
